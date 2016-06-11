9522fb97922aa67a7f4fc0ce8dd8b1318a199361
Marcin Burgknap <marcin.burgknap@gmail.com>
2016-06-11 20:58:45 +0200

    update wsparcie slack

--- RedmineLog/Utils/AppLog.cs
+++ RedmineLog/Utils/AppLog.cs
@@ -1,15 +1,75 @@
-﻿using Ninject;
+﻿using Newtonsoft.Json;
+using Ninject;
 using NLog;
+using NLog.Internal;
 using RedmineLog.Common;
 using RedmineLog.UI.Common;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RedmineLog
 {
+    public class SlackClient
+    {
+        private readonly Uri _uri;
+        private readonly Encoding _encoding = new UTF8Encoding();
+
+        public SlackClient(string urlWithAccessToken)
+        {
+            _uri = new Uri(urlWithAccessToken);
+        }
+
+        //Post a message using simple strings
+        public void PostMessage(string text, string username = null, string channel = null)
+        {
+            Payload payload = new Payload()
+            {
+                Channel = channel,
+                Username = username,
+                Text = text
+            };
+
+            PostMessage(payload);
+        }
+
+        //Post a message using a Payload object
+        public void PostMessage(Payload payload)
+        {
+            string payloadJson = JsonConvert.SerializeObject(payload);
+
+            using (WebClient client = new WebClient())
+            {
+                NameValueCollection data = new NameValueCollection();
+                data["payload"] = payloadJson;
+
+                var response = client.UploadValues(_uri, "POST", data);
+
+                //The response text is usually "ok"
+                string responseText = _encoding.GetString(response);
+            }
+        }
+    }
+
+    //This class serializes into the Json payload required by Slack Incoming WebHooks
+    public class Payload
+    {
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+
     class AppLog : ILog
     {
         private static Logger Log;
@@ -30,6 +90,23 @@ namespace RedmineLog
         {
             Log.Error(inTag, ex);
             NotifyBox.Show(inMessage, inTitle);
+
+            try
+            {
+                string urlWithAccessToken = System.Configuration.ConfigurationManager.AppSettings["SlackChannel"];
+
+                SlackClient client = new SlackClient(urlWithAccessToken);
+
+                String msg = "Error " + ex.Message + Environment.NewLine + ex.StackTrace;
+
+                client.PostMessage(username: "RedmineLog ",
+                           text: msg,
+                           channel: "#redmine_log");
+            }
+            catch (Exception ex2)
+            {
+                Log.Error(inTag, ex2);
+            }
         }
     }
 }
